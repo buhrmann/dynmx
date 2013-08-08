c8fe3d9c925e840c952799d4749110f29fea455a
Thomas Buhrmann <thomas.buehrmann@gmail.com>
2013-08-08 17:40:37 +0200

    Added metabolising minimal SMC agent.

---
+++ src/dynmx/SMCAgentViz1d.h
@@ -0,0 +1,14 @@
+//
+//  SMCAgentViz1d.h
+//  dynmx
+//
+//  Created by Thomas Buhrmann on 6/24/13.
+//
+//
+
+#ifndef dynmx_SMCAgentViz1d_h
+#define dynmx_SMCAgentViz1d_h
+
+
+
+#endif
---
+++ src/dynmx/SMCView1d.h
@@ -0,0 +1,14 @@
+//
+//  SMCView1d.h
+//  dynmx
+//
+//  Created by Thomas Buhrmann on 6/24/13.
+//
+//
+
+#ifndef dynmx_SMCView1d_h
+#define dynmx_SMCView1d_h
+
+
+
+#endif
