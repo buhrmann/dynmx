cbd8334ef7894400749f737199ccc290749216c7
Thomas Buhrmann <thomas.buehrmann@gmail.com>
2013-10-11 13:03:23 +0200

    Correct run script.

--- bin/run.sh
+++ bin/run.sh
@@ -4,7 +4,10 @@
 ./dynmx "$1" > log.txt &
 
 # Append log with subsequent ones
-for ((i=0;i<$2;i++)) 
-  do sleep 2s
-  ./dynmx Configs/SMCArmEvo.xml >> log.txt
+for ((i=0;i<"$2"-1;i++)) 
+do
+  sleep 2s
+  ./dynmx Configs/SMCArmEvo.xml >> log.txt &
 done
+
+less +F log.txt
