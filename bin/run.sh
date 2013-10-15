612c81408ae3add83c8b8a8214fb2fe5d663dc63
Thomas Buhrmann <thomas.buehrmann@gmail.com>
2013-10-15 14:50:28 +0200

    Added console feedback to run script.

--- bin/run.sh
+++ bin/run.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/bin/bash 
 
 exe="../xcode4/build/Release/dynmx.app/Contents/MacOS/dynmx"
 
 # Start first with new log file
+i=0
+echo Starting process $i
 $exe "$1" > ./log.txt &
 
 # Append log with subsequent ones
-for ((i=0;i<"$2"-1;i++)) 
+for ((i=1;i<"$2";i++)) 
 do
   sleep 2s
+  echo Starting process $i
   $exe "$1" >> ./log.txt &
 done
 
