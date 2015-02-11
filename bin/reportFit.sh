38c2c2781fd486d33e8d863fa7f687f02af4bc22
Thomas Buhrmann <thomas.buehrmann@gmail.com>
2015-02-11 12:28:26 +0100

    Single sensor phototaxis with disruptions.

--- bin/reportFit.sh
+++ bin/reportFit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for folder in $1/*
 do
-  f="$folder/Evolvable.xml"
+  f="$folder/GA_BestGenome.xml"
   fit=$(grep "Genome Fitness" "$f")
   fnm=$(basename $folder)
   echo "$fnm: $fit"
